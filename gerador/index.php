5acd46a4a777e8a020ba92992f5f4c337a36911a
Junior Anzolin <junioranzolin58@gmail.com>
2018-06-24 23:15:40 -0300

    Inicio do projeto

---
+++ gerador/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.scss">
+    <link rel="stylesheet" type="text/css" href="../global-style.scss">
+
+    <title>Gerar Curriculo</title>
+</head>
+<body>
+    teste
+</body>
+</html>
